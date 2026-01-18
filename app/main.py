d69f65be6cbd68dd7c6fdefe5b1c081df9f9267b
Yeisson Ochoa <yeisson.ochoa72@gmail.com>
2026-01-18 11:36:09 -0500

    Habilitar CORS para consumo de API desde Angular

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from app.db.config import engine, Base
 from app.routes import gasto_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="API de Gastos",
     version="1.0.0"
+)
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200",  # Angular
+        "http://localhost:8501",  # Streamlit
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Agregar rutas
